4f6b0409e735900457c946454301344b3687718f
rifkegribenes <rifkegribenes@gmail.com>
2020-04-23 22:30:48 -0700

    add build

---
+++ client/build/precache-manifest.58505a3735e3c4542bc8f45e37c1ec5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328ee4050c4f2995a792474fcb6962fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2679fb1644e6800c0545",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee004e3b72a40fb15279",
+    "url": "/static/js/2.580d99fb.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.580d99fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2679fb1644e6800c0545",
+    "url": "/static/js/main.eb29d7f4.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
